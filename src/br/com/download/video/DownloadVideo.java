65a17c586a220445cd7f90697c2407b7b03f86da
Luiz H G Leite <luiz.hleite@hotmail.com>
2019-06-21 14:21:18 -0300

    first commit

---
+++ src/br/com/download/video/DownloadVideo.java
@@ -0,0 +1,84 @@
+package br.com.download.video;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DownloadVideo {
+
+    public static String download_path = "D:\\Git\\Download_Youtube_VID";
+
+    public static String fileName = "videos.txt";
+
+    public static void main(String[] args) {
+
+        getListaVideos().forEach(urlVideo -> {
+            getdData(urlVideo, getParams());
+        });
+
+    }
+
+    private static List<String> getListaVideos() {
+
+        List<String> listaUrlVideos = new ArrayList<>();
+
+        try {
+            File file = new File(fileName);
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String st;
+            while ((st = br.readLine()) != null) {
+                listaUrlVideos.add(st);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return listaUrlVideos;
+    }
+
+    private static String getParams() {
+        return "" +
+                " -U" +
+                //" --write-all-thumbnails" +
+                //" --write-description" + ESTAS INFOS VEM NO JSON
+                " --write-info-json" +
+                " --write-auto-sub" +
+                " --write-sub" +
+                " --sub-format vtt" +
+                " --sub-lang pt" +
+//                " -o " + download_path + "\\videos\\%(title)s.%(ext)s" +
+                " -o " + download_path + "\\videos\\%(id)s.%(ext)s" +
+                " --recode-video mp4" +
+                " --no-mtime" +
+                //" -F" +
+                " -f 135" +
+                " --convert-subs srt";
+
+    }
+
+    private static void getdData(String url, String params) {
+
+        String[] command =
+                {
+                        "cmd"
+                };
+
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            new Thread(new SyncBuffer(p.getErrorStream(), System.err)).start();
+            new Thread(new SyncBuffer(p.getInputStream(), System.out)).start();
+            PrintWriter stdin = new PrintWriter(p.getOutputStream());
+            stdin.println("cd \"" + download_path + "\"");
+            stdin.println("youtube-dl " + url + params);
+            stdin.close();
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}	
---
+++ src/br/com/download/video/SyncBuffer.java
@@ -0,0 +1,25 @@
+package br.com.download.video;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+class SyncBuffer implements Runnable {
+    private final OutputStream ostrm_;
+    private final InputStream istrm_;
+
+    public SyncBuffer(InputStream istrm, OutputStream ostrm) {
+        istrm_ = istrm;
+        ostrm_ = ostrm;
+    }
+
+    public void run() {
+        try {
+            final byte[] buffer = new byte[1024];
+            for (int length = 0; (length = istrm_.read(buffer)) != -1; ) {
+                ostrm_.write(buffer, 0, length);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
